2fe4f1db999416d70249083844ea8f823acd7bbb
Sean Erfan <seanerfan@gmail.com>
2024-02-03 16:02:10 -0500

    Question sample

---
+++ src/components/Question.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Question = () => {
+  return (
+    <div class="question">
+        <h6>Recitation question</h6>
+        <p>Who should i email regarding my recitation score, the TAs from my recitation and cc the professor?</p>
+    </div>
+  )
+}
+
+export default Question
