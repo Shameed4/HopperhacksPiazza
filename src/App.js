64b3d7f05101fea5498f4ab173bc39e052215742
Mattzh0 <110125986+Mattzh0@users.noreply.github.com>
2024-02-03 17:26:37 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import Question from './components/Question';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Classes from './Classes';
+
 
 function App() {
+  const redirectToClasses = () => {
+    window.location.href = '/classes';
+  };
+
   return (
-    <div className="App">
-      <Question></Question>
-      <Question></Question>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <button onClick={redirectToClasses}>Login</button>
+              </div>
+            }
+          />
+          <Route path="/classes" element={<Classes />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Classes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Classes.css';
+
+const Classes = () => {
+  return (
+    <div class="classes-container">
+      <h1>Classes</h1>
+      <button class="class-button">CSE216</button>
+      <button class="class-button">CSE220</button>
+      <button class="class-button">CSE316</button>
+    </div>
+  )
+}
+
+export default Classes
