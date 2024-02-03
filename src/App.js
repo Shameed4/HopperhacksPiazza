d0fcc96f24f3f33df7fcaacc133d2c8cf02c635d
Mattzh0 <110125986+Mattzh0@users.noreply.github.com>
2024-02-03 17:58:15 -0500

    link to dashboard

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Question from './components/Question';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Classes from './pages/Classes/Classes';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 
 function App() {
   const redirectToClasses = () => {
     window.location.href = '/classes';
+  };
+  const redirectToDashboard = () => {
+    window.location.href = '/dashboard';
   };
 
   return (
@@ -20,10 +24,12 @@ function App() {
               element={
                 <div>
                   <button onClick={redirectToClasses}>Login</button>
+                  <button onClick={redirectToDashboard}>Dashboard</button>
                 </div>
               }
             />
             <Route path="/classes" element={<Classes />} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
         </div>
       </Router>
