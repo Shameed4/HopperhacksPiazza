1594a3d90913373dae4dc7d473d6de51ceedebc7
Sean Erfan <seanerfan@gmail.com>
2024-02-03 18:40:12 -0500

    Home page is router, classes page  glitching

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import Question from './components/Question';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
 import Classes from './pages/Classes/Classes';
 import Dashboard from './pages/Dashboard/Dashboard';
 
 
 function App() {
+  const redirectToLogin = () => {
+    window.location.href = '/login';
+  };
   const redirectToClasses = () => {
     window.location.href = '/classes';
   };
@@ -15,42 +19,25 @@ function App() {
   };
 
   return (
-    <>
-    <div class="gradient"></div>
-    <div class="login">
-        <h4>Login</h4>
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Username" aria-label="Recipient's username"
-                aria-describedby="basic-addon2"/>
-        </div>
-        <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Password" aria-label="Recipient's username"
-                aria-describedby="basic-addon2"/>
-        </div>
-        <button type="button" class="btn btn-primary">Login</button>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <div>
-                  <button onClick={redirectToClasses}>Login</button>
-                  <button onClick={redirectToDashboard}>Dashboard</button>
-                </div>
-              }
-            />
-            <Route path="/classes" element={<Classes />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </div>
-      </Router>
-    </>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <button onClick={redirectToLogin}>Login</button>
+                <button onClick={redirectToClasses}>Classes</button>
+                <button onClick={redirectToDashboard}>Dashboard</button>
+              </div>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/classes" element={<Classes />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
