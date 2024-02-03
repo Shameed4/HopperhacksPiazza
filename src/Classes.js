f2c7372c0e13e0ee825641771b0ce5cd28e07505
Mattzh0 <110125986+Mattzh0@users.noreply.github.com>
2024-02-03 17:51:00 -0500

    Update Classes.js

--- src/Classes.js
+++ src/Classes.js
@@ -4,7 +4,7 @@ import './Classes.css';
 const Classes = () => {
   return (
     <div class="classes-container">
-      <h1>Classes</h1>
+      <h1>Select Your Class</h1>
       <button class="class-button">CSE216</button>
       <button class="class-button">CSE220</button>
       <button class="class-button">CSE316</button>
