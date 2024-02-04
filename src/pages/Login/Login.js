db73774124fba313ff7a8ab30e9406108e28eb97
R I T E S H <81812754+ritessshhh@users.noreply.github.com>
2024-02-03 19:01:26 -0500

    something

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,21 +4,24 @@ import "./Login.css"
 const Login = () => {
   return (
     <>
-    <div class="gradient"></div>
-    <div class="login">
-        <h4>Login</h4>
-        <div class="input-group mb-3">
+      <div class="gradient"></div>
+      <div className='containerLogin'>
+        <h4>Welcome to MARS!</h4>
+        <div class="login">
+          <h4>Login</h4>
+          <div class="input-group mb-3">
             <input type="text" class="form-control" placeholder="Username" aria-label="Recipient's username"
-                aria-describedby="basic-addon2"/>
-        </div>
-        <div class="input-group mb-3">
+              aria-describedby="basic-addon2" />
+          </div>
+          <div class="input-group mb-3">
             <input type="password" class="form-control" placeholder="Password" aria-label="Recipient's username"
-                aria-describedby="basic-addon2"/>
+              aria-describedby="basic-addon2" />
+          </div>
+          <button type="button" class="btn btn-primary">Login</button>
         </div>
-        <button type="button" class="btn btn-primary">Login</button>
-    </div>
+      </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
     </>
